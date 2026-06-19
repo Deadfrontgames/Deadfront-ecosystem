d86cfd72a789f76d07e32dcbf733c53c88483c9f
Deadfrontgames <enfikisarip@gmail.com>
2026-06-19 11:37:57 +0700

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import Gameplay from "@/components/Gameplay";
+import Rewards from "@/components/Rewards";
+import Roadmap from "@/components/Roadmap";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <Gameplay />
+      <Rewards />
+      <Roadmap />
+    </>
+  );
+}
